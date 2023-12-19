c96a9f30b73b2b08655a50cb8548d8c11c701767
venurawarnasooriya <venura@ashogu.com>
2023-12-19 05:42:47 +0530

    footer-component done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet, RouterModule } from "@angular/router";
 
+//components
+import { NavBarComponent } from "./Components/Multiple Use/nav-bar/nav-bar.component";
+import { FooterComponent } from "./Components/Multiple Use/footer/footer.component";
+
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule],
+  imports: [CommonModule, RouterOutlet, RouterModule, NavBarComponent, FooterComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
