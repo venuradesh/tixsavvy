9ce0be68a95a83149440991496ea90fb23f47ee9
venurawarnasooriya <venura@ashogu.com>
2023-12-19 13:33:04 +0530

    event-details-page done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./Pages/home/home.component";
+import { EventDetailsPageComponent } from "./Pages/event-details-page/event-details-page.component";
 
 export const routes: Routes = [
   {
     path: "",
     pathMatch: "full",
     component: HomeComponent,
+  },
+  {
+    path: "event",
+    pathMatch: "full",
+    component: EventDetailsPageComponent,
   },
 ];
